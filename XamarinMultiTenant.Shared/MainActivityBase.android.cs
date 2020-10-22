6a76cfacdeeb1c0eae560504edd2ab52b123f9fe
Alexandre Costa <alexandresantoscosta@gmail.com>
2020-10-22 13:21:51 -0300

    Implementing multi targeting to reduce the number of projects

---
+++ XamarinMultiTenant.Shared/MainActivityBase.android.cs
@@ -0,0 +1,54 @@
+﻿using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Prism;
+using Prism.Ioc;
+using XamarinMultiTenant.Common;
+
+namespace XamarinMultiTenant.Android.Core
+{
+    //[Activity(Label = "XamarinMultiTenant", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public abstract class MainActivityBase : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected abstract ITenant Tenant { get; }
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            ResourceIdManager.UpdateIdValues();
+
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            Xamarin.Essentials.Platform.Init(this, bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            global::Xamarin.Forms.FormsMaterial.Init(this, bundle);
+            LoadApplication(new App(new AndroidInitializer(Tenant)));
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    public class AndroidInitializer : IPlatformInitializer
+    {
+        private readonly ITenant tenant;
+
+        public AndroidInitializer(ITenant tenant) : base()
+        {
+            this.tenant = tenant;
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterInstance<ITenant>(tenant);
+        }
+    }
+}
+
